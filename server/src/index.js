b3e9425acfd5706ed54928ce298bd699bd691c05
nicolelinh444 <nicolelinh444@gmail.com>
2025-11-10 13:54:26 -0800

    completed post requests on index.js

--- server/src/index.js
+++ server/src/index.js
@@ -67,9 +67,30 @@ async function deleteOneAnimal(id) {
 
 // 6. addOneAnimal(name, category, can_fly, lives_in)
 
+async function addOneAnimal(name, category, can_fly, lives_in) {
+  // we declared db in our boilerplate code, which connects us to the SQL database
+  // The db.query() method lets us write SQL code to query the database. Takes in 2 parameters:
+  // 1. The SQL command
+  // 2. An array that contains dynamic values that we inject into the SQL command
+  // the SQL command needs to be written all in one line, using $1 - $4 as placeholders for dynamic values
+  await db.query(
+    "INSERT INTO animals (name, category, can_fly, lives_in) VALUES ($1, $2, $3, $4)",
+    [name, category, can_fly, lives_in]
+  );
+}
+
 // 7. updateOneAnimalName(id, newName)
+async function updateOneAnimalName(id, newName) {
+  await db.query("UPDATE animals SET name = $1 WHERE id = $2", [newName, id]);
+}
 
 // 8. updateOneAnimalCategory(id, newCategory)
+async function updateOneAnimalCategory(id, newCategory) {
+  await db.query("UPDATE animals SET category = $1 WHERE id = $2", [
+    newCategory,
+    id,
+  ]);
+}
 
 // ---------------------------------
 // API Endpoints
@@ -113,6 +134,60 @@ app.post("/delete-one-animal/:id", async (req, res) => {
 
 // 6. POST /add-one-animal
 
+// app.post() takes in two parameters:
+//  1. The URL path for the endpoint
+//  2. The callback function that gets triggered when we send a request to the endpoint URL
+app.post("/add-one-animal", async (req, res) => {
+  // we access whatever was sent in the request body and save it in this variable
+  const { name, category, can_fly, lives_in } = req.body;
+
+  // calling the helper function
+  // we aren't declaring a variable because our helper function doesn't need to return anything
+  await addOneAnimal(name, category, can_fly, lives_in);
+
+  // Sending a response as text, so we use res.send() which sends text
+  // If we wanted to send a response as JSON data (which would need to be an object or array to be valid JSON), we would use res.json()
+  res.send(`Success! Animal was added.`);
+});
+
 // 7. POST /update-one-animal-name
+app.post("/update-one-animal-name", async (req, res) => {
+  const { id, newName } = req.body;
+
+  await updateOneAnimalName(id, newName);
+
+  res.send(`Success! The animal's name was updated.`);
+});
+
+app.post("/update-one-animal-name-with-error-handling", async (req, res) => {
+  try {
+    // Possible errors:
+    // DONE: 400 Bad Request: what should we do when there's no body?
+    // 500 Internal Server Error: when a unique constraint is violated
+    // 404 Resource Not Found: using camelCase for the api endpoint
+    // 404 Resource Not Found: no existing animal was found with the given id
+
+    const { id, newName } = req.body;
+
+    // check for missing required fields in the request body: id and newName
+    if (!newName || !id) {
+      // return error message with 400 Bad Request status code, because the request was badly formed with wrong syntax.
+      // All 4xx status codes are client-side errors, which means the client sent a bad request
+      return res.status(400).send("Error: Missing required fields");
+    }
+
+    await updateOneAnimalName(id, newName);
+
+    res.send(`Success! The animal's name was updated.`);
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 // 8. POST /update-one-animal-category
+
+app.post("/update-one-animal-category", async (req, res) => {
+  const { id, newCategory } = req.body;
+  await updateOneAnimalCategory(id, newCategory);
+  res.send(`Success! The animal's category was updated.`);
+});
